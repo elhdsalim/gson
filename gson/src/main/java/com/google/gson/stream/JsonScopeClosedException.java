266ad758f29c6f9fd7c7ebc624723acc1fd63741
slmhd <salim.elhadad.etu@univ-lille.fr>
2026-04-05 16:37:29 +0200

    fix warnings

--- gson/src/main/java/com/google/gson/stream/JsonScopeClosedException.java
+++ gson/src/main/java/com/google/gson/stream/JsonScopeClosedException.java
@@ -2,6 +2,9 @@ package com.google.gson.stream;
 
 public final class JsonScopeClosedException extends IllegalStateException {
 
+    // to avoid error : "serializable class com.google.gson.stream.JsonScopeClosedException has no definition of serialVersionUID"
+    private static final long serialVersionUID = 1L;
+
     public JsonScopeClosedException(String message) {
         super(message);
     }
--- gson/src/main/java/com/google/gson/stream/NotHandledJsonValueException.java
+++ gson/src/main/java/com/google/gson/stream/NotHandledJsonValueException.java
@@ -4,6 +4,9 @@ import java.io.IOException;
 
 public final class NotHandledJsonValueException extends IOException {
 
+    // to avoid error : "serializable class com.google.gson.stream.NotHandledJsonValueException has no definition of serialVersionUID"
+    private static final long serialVersionUID = 1L;
+
     public NotHandledJsonValueException(String message) {
         super(message);
     }
